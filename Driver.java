474513f691006be66e0b037a8f59ba732d011f1f
Mike Litman <mike@awesomefat.com>
2015-04-23 11:31:19 -0500

    parse-stmt solution

--- Driver.java
+++ Driver.java
@@ -1,11 +1,10 @@
-
 public class Driver 
 {
 	public static void main(String[] args)
 	{
 		Parser p = new Parser("     a =     (a *   b) - c;");
 		Parser p2 = new Parser("     a = b - c;");
-
 		p.parse();
+		p2.parse();
 	}
 }
--- Parser.java
+++ Parser.java
@@ -3,10 +3,13 @@ public class Parser
 {
 	private String theStmt;
 	private int pos; //where am I in the theStmt string
+	private static final String legalVariableCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "; 
+	private static final String legalOpCharacters = "+-*/% ";
 	
 	public Parser(String theStmt)
 	{
 		this.theStmt = theStmt;
+		
 		this.pos = 0;
 	}
 	
@@ -15,25 +18,71 @@ public class Parser
 		this.parse_stmt();
 	}
 	
+	private String getNextToken(String legalChars)
+	{
+		String token = "";
+		while(pos < this.theStmt.length())
+		{
+			if(legalChars.indexOf(this.theStmt.charAt(pos)) != -1)
+			{
+				token += this.theStmt.charAt(pos);
+			}
+			else
+			{
+				//this means we are at the end of the token
+				//We are always trimming leading and trailing spaces
+				//move forward one
+				break;
+			}
+			pos++;
+		}
+		return token.trim();
+	}
+	
 	private void parse_stmt()
 	{
 		//Print each time it reads something like:
 		// Read: VarName = a
-		// Reading: Math-Expr
+		String varName = this.getNextToken(Parser.legalVariableCharacters);
+		System.out.println("Read VarName: " + varName);
 		
-		//read a var name
-		//read a math_expr
+		//burn past the =
+		this.getNextToken("=");
+		
+		// Reading: Math-Expr
+		this.parse_math_expr();
+		
+		//burn past the ;
+		this.getNextToken(";");
 	}
 	
 	private void parse_math_expr()
 	{
-		//Display
-		//Reading Left:
-		//Read OP = *
-		//Reading Right
-		
-		//read left
-		//read op
-		//read right
+		String varName = this.getNextToken(Parser.legalVariableCharacters);
+		if(varName.length() == 0)
+		{
+			//we know that we are at the beginning of a paren-math-expr
+			this.getNextToken("(");
+			this.parse_math_expr();
+			this.getNextToken(")");
+		}
+		else
+		{
+			System.out.println("Read VarName: " + varName);
+		}
+		String op = this.getNextToken(Parser.legalOpCharacters);
+		System.out.println("Read Op: " + op);
+		varName = this.getNextToken(Parser.legalVariableCharacters);
+		if(varName.length() == 0)
+		{
+			//we know that we are at the beginning of a paren-math-expr
+			this.getNextToken("(");
+			this.parse_math_expr();
+			this.getNextToken(")");
+		}
+		else
+		{
+			System.out.println("Read VarName: " + varName);
+		}
 	}
 }
